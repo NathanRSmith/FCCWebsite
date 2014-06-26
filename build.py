7e83fe8b25969f5c86a300649927bc083278cbe8
Nathan Smith <nathanrandal@hotmail.com>
2014-06-26 13:35:03 -0400

    changed to only render templates ending in ".html"

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ def build():
     BOWER_DIR = 'bower_components'
     STATIC_DIR = 'static'
     MEDIA_DIR = 'media'
-    
+
     # make build directory
     if not os.path.exists(BUILD_DIR):
         os.makedirs(BUILD_DIR)
@@ -24,48 +24,49 @@ def build():
         sroot = root.split(os.sep)
         for d in dirs:
             os.mkdir(os.path.join(BUILD_DIR, d))
-            
+
         for template in files:
-            path_rel_build = sroot[len(PAGES_DIR.split(os.sep)):]
-            root_url_list = [os.pardir]*len(path_rel_build)
-            root_url = '/'.join(root_url_list)
-            
-            context = {
-                'root': root_url+'/' if len(root_url)>0 else root_url,
-                'static': '/'.join(root_url_list+[STATIC_DIR, '']),
-                'media': '/'.join(root_url_list+[MEDIA_DIR, '']),
-                'bower': '/'.join(root_url_list+[BOWER_DIR, '']),
-            }
-            
-            with open(os.path.join(*([BUILD_DIR]+path_rel_build+[template])), 'w') as f:
-                try:
-                    print 'Rendering %s' % os.path.join( *(sroot[1:]+[template]) )
-                    f.write(
-                        env.get_template(
-                            os.path.join(
-                                *(sroot[1:]+[template])
-                            )
-                        ).render(context)
-                    )
-                except:
-                    print 'ERROR: %s'%os.path.join(root, template)
-                    raise
-    
+            if template.endswith('.html'):
+                path_rel_build = sroot[len(PAGES_DIR.split(os.sep)):]
+                root_url_list = [os.pardir]*len(path_rel_build)
+                root_url = '/'.join(root_url_list)
+
+                context = {
+                    'root': root_url+'/' if len(root_url)>0 else root_url,
+                    'static': '/'.join(root_url_list+[STATIC_DIR, '']),
+                    'media': '/'.join(root_url_list+[MEDIA_DIR, '']),
+                    'bower': '/'.join(root_url_list+[BOWER_DIR, '']),
+                }
+
+                with open(os.path.join(*([BUILD_DIR]+path_rel_build+[template])), 'w') as f:
+                    try:
+                        print 'Rendering %s' % os.path.join( *(sroot[1:]+[template]) )
+                        f.write(
+                            env.get_template(
+                                os.path.join(
+                                    *(sroot[1:]+[template])
+                                )
+                            ).render(context)
+                        )
+                    except:
+                        print 'ERROR: %s'%os.path.join(root, template)
+                        raise
+
     # copy static files
     print 'Copying static files'
     shutil.copytree('static', os.path.join(BUILD_DIR, 'static'))
-    
+
     # copy media files
     print 'Copying media files'
     shutil.copytree('media', os.path.join(BUILD_DIR, 'media'))
-    
+
     # copy bower resources
     if os.path.exists('bower_components'):
         print 'Copying bower files'
         shutil.copytree('bower_components', os.path.join(BUILD_DIR, 'bower_components'))
 
     print 'Done'
-                          
+
 
 if __name__ == '__main__':
     build()
